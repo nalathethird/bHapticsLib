4f8d2c7d8418f3da936642ac366070e2f39f84f4
NalaTheThird <36301692+nalathethird@users.noreply.github.com>
2025-10-17 16:28:06 -0500

    Hotfix for Version Changes - Whoops

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -5,7 +5,7 @@
         public const string Name = "bHapticsLib";
         public const string Author = "Herp Derpinstine, NalaTheThird";
         public const string Company = "Lava Gang";
-        public const string Version = "1.0.7";
+        public const string Version = "1.0.8";
         public const string DownloadLink = "https://github.com/nalathethird/bHapticsLib";
     }
 }
